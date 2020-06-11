66bc52bb0771ef2d8e40bfac257ad11b34bfcc13
ajy <ajy@ajys-MacBook-Pro.local>
2020-06-11 21:46:42 +0000

    Added static files including preferences, and updated some other files.

--- Makefile
+++ Makefile
@@ -1,6 +1,10 @@
 BREW_PACKAGES = brew.txt
 PYTHON_PACKAGES = python.txt
 
+get_brew:
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+get_amethyst:
+	brew cask install amethyst
 brew:
 	brew install $$(<$(BREW_PACKAGES))
 python:
--- scripts/install/apps.py
+++ scripts/install/apps.py
@@ -2,25 +2,35 @@ from yaml import safe_load
 import wget
 from os.path import exists
 from os import mkdir
+from multiprocessing import Pool
+
+
+apps_yaml_path = './static/apps.yml'
+output_dir = './downloaded_apps/'
+n_threads = 7
+
+
+def download_package(contents, key):
+    app_name = key
+    properties = contents[key]
+    try:
+        url = properties['url']
+        extension = properties.get('extension')
+        print(f'Installing {app_name}.')
+        wget.download(url, f'{output_dir}')
+    except:
+        print(f'An error ocurred when trying to download {app_name}')
+
 
 def main():
-    apps_yaml_path = './apps.yml'
-    output_dir = './downloaded_apps/'
-
     with open(apps_yaml_path) as file_:
         contents = safe_load(file_)
 
     if not exists(output_dir):
         mkdir(output_dir)
 
-    for app_name, properties in contents.items():
-        try:
-            url = properties['url']
-            extension = properties.get('extension')
-            print(f'Installing {app_name}.')
-            wget.download(url, f'{output_dir}')#/{app_name}')#.{extension}')
-        except:
-            print(f'An error ocurred when trying to download {app_name}')
+    with Pool(n_threads) as pool:
+        pool.starmap(download_package, [(contents, key) for key in contents.keys()])
 
 if __name__ == '__main__':
     main()
